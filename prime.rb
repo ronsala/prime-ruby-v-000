280e0a8199a9f9545779f6a1112eda4471476dea
Ron Sala <ron.sala@gmail.com>
2017-12-27 22:46:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,30 +1,19 @@
 def prime?(num)
   return false if num <= 1
-  for i in (2...num) do
-    if num % i == 0
-      false
-    else
-      true
-    end
+  ary = (2...num).to_a
+  if ary.none? { |i| num % i == 0 }
+    true
+  else
+    false
   end
 end
 
-# def prime?(num)
-#   return false if num <= 1
-#   ary = (2...num).to_a
-#   if ary.none? { |i| num % i == 0 }
-#     true
-#   else
-#     false
-#   end
-# end
-#
-# def prime?(num)
-#   return false if num <= 1
-#   (2...num).none? { |i| num % i == 0 } ? true : false
-# end
-#
-# def prime?(num)
-#   return false if num <= 1
-#   (2...num).any? { |i| num % i == 0 } ? false : true
-# end
+def prime?(num)
+  return false if num <= 1
+  (2...num).none? { |i| num % i == 0 } ? true : false
+end
+
+def prime?(num)
+  return false if num <= 1
+  (2...num).any? { |i| num % i == 0 } ? false : true
+end
